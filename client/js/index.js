e20dd6e56c3677909f96595a5d1a5a73ceb6dbc2
Roberto Sanchez <rsanchez92@live.com>
2019-02-08 20:55:02 -0800

    Played around with SocketIO emit feature

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,17 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server.')
+    
+    socket.emit('createMessage', {
+        from: 'Roberto Sanchez',
+        text:'Yeah sure'
+    });
 });
+
 socket.on('disconnect', function () {
     console.log('Disconnected from server.')
-})
+});
+
+socket.on('newMessage', function(message){
+    console.log('newMessage',message);
+});
--- server/server.js
+++ server/server.js
@@ -14,11 +14,27 @@ var io = socketIO(server)
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
-    console.log('New User connected.')
+    console.log('New User connected.');
+    socket.emit('newMessage',{
+        from: 'Jason',
+        text: 'lol',
+        createdAt: new Date().getTime()
+    });
+    
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage: ',message);
+
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
     socket.on('disconnect',()=>{
         console.log('Disconnected from client.')
-    })
-})
+    });
+});
 
 server.listen(port,()=>{
     console.log(`Server is up on ${port}`)
