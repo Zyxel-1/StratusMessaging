6804db11d5ddfc89fbf2aeadf545af89c596be63
Roberto Sanchez <rsanchez92@live.com>
2019-02-10 12:13:26 -0800

    Added Join page

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
 socket.on('connect', function () {
     console.log('Connected to server.');
 });
@@ -9,17 +24,18 @@ socket.on('disconnect', function () {
 });
 
 // Displays new messages in chat
-socket.on('newMessage', function(message){
+socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a')
     var template = jQuery('#message-template').html();
-    
-    var html = Mustache.render(template,{
+
+    var html = Mustache.render(template, {
         text: message.text,
         from: message.from,
         createdAt: formattedTime
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 // Display locations in chat
@@ -33,15 +49,16 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 // Sends message to Server
-jQuery('#message-form').on('submit', function (e){
+jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
-    
+
     var messageTextBox = jQuery('[name=message]');
     // Emits message in input box
-    socket.emit('createMessage',{
+    socket.emit('createMessage', {
         from: 'User',
         text: messageTextBox.val()
     }, function () {
@@ -52,23 +69,23 @@ jQuery('#message-form').on('submit', function (e){
 // Getting user's location
 var locationButton = jQuery('#send-location');
 
-locationButton.on('click',function (){
-    if(!navigator.geolocation){
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
 
-    locationButton.attr('disabled','disabled').text('Sending Location...');
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
 
-    navigator.geolocation.getCurrentPosition(function (position){
-        
+    navigator.geolocation.getCurrentPosition(function (position) {
 
-        socket.emit('createLocationMessage',{
+
+        socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
         locationButton.removeAttr('disabled').text('Send Location Here')
 
-    }, function (){
+    }, function () {
         locationButton.removeAttr('disabled').text('Send Location Here')
         alert('Unable to fetch location.');
     });
